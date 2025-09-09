09e7bebd3f07ffd193d3a7b55f377fd97c4dde62
colinberrygithub <97005473+colinberrygithub@users.noreply.github.com>
2025-09-09 08:07:54 +0100

    Update int_question_tags.sql

--- models/intermediate/int_question_tags.sql
+++ models/intermediate/int_question_tags.sql
@@ -1,9 +1,13 @@
-{{ config(tags=['intermediate']) }}
-with base as (
+-- models/intermediate/int_question_tags.sql
+{{ config(materialized='view') }}
+
+with exploded as (
   select
     q.question_id,
     lower(tag) as tag_name
-  from {{ ref('stg_stackoverflow__posts_questions') }} q,
-  unnest(regexp_extract_all(q.tags, r'<([^>]+)>')) as tag
+  from {{ ref('stg_stackoverflow__posts_questions') }} as q,
+  unnest(split(q.tags, '|')) as tag
+  where q.tags is not null
+    and tag <> ''
 )
-select * from base
+select * from exploded
